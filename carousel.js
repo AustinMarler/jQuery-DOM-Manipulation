6406cb573aabab93b7ca4f91492ffa89be9a5084
Austin Marler <austinnmarler@gmail.com>
2019-03-26 15:46:12 -0700

    added background styling

--- carousel.js
+++ carousel.js
@@ -1,21 +1,21 @@
 var carouselImages = [
     {
-        image_url: 'http://interfacelift.com/wallpaper/Debbfb5b/03966_upondescent_1280x720.jpg',
+        image_url: 'https://2.bp.blogspot.com/-wO-RchFmTF0/UXlcoF5jtvI/AAAAAAAAJY8/d4fM9IgzPJE/s1600/clouds_2.png',
         title: 'Upon Descent',
         photographer_id: 'Phil2001'
     },
     {
-        image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03965_sunsetinthecascades_1280x720.jpg',
+        image_url: 'https://finerywebdesign.com/wp-content/uploads/2018/12/bob-ross-hairstyle-outstanding-bob-ross-style-ipad-pro-drawing-with-apple-pencil-ebb0a5eba19cec8aa4-ec95a0ed948c-of-bob-ross-hairstyle.jpg',
         title: 'Sunset in the Cascades',
         photographer_id: 'smpoutdoors'
     },
     {
-        image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03958_seljalandsfossiceland_1280x720.jpg',
+        image_url: 'https://i.ytimg.com/vi/W2bWjbAACZc/maxresdefault.jpg',
         title: 'Seljalandsfoss, Iceland',
         photographer_id: 'Robin Kamp'
     },
     {
-        image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03950_godsrays_1280x720.jpg',
+        image_url: 'https://i.ytimg.com/vi/KhccNW-UpRM/maxresdefault.jpg',
         title: 'Gods Rays',
         photographer_id: 'illuminati'
     }
