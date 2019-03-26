18d48f446e943f39b1ebd7681bdc8a77e172e550
Austin Marler <austinnmarler@gmail.com>
2019-03-26 14:38:35 -0700

    accordion working, animations incomplete

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+    $('#accordion').on('click', 'h4', function(){
+
+        var content = $(this).siblings('.accordion-content');
+
+        if (content.hasClass('accordion-content-show')) {
+            content.addClass('accordion-content-hidden');
+            content.removeClass('accordion-content-show');
+        } else {
+            $('.accordion-content').addClass('accordion-content-hidden');
+            content.addClass('accordion-content-show');
+            content.removeClass('accordion-content-hidden');
+        }
+
+    });
+    
 })
