6039d711890cde0ebce19a92ddb7024629f39a90
Austin Marler <austinnmarler@gmail.com>
2019-03-27 08:50:37 -0700

    final edit

--- carousel.js
+++ carousel.js
@@ -1,22 +1,22 @@
 var carouselImages = [
     {
         image_url: 'https://2.bp.blogspot.com/-wO-RchFmTF0/UXlcoF5jtvI/AAAAAAAAJY8/d4fM9IgzPJE/s1600/clouds_2.png',
-        title: 'Upon Descent',
-        photographer_id: 'Phil2001'
+        title: 'Excursion on the Sea',
+        artist: 'Bob Ross'
     },
     {
         image_url: 'https://finerywebdesign.com/wp-content/uploads/2018/12/bob-ross-hairstyle-outstanding-bob-ross-style-ipad-pro-drawing-with-apple-pencil-ebb0a5eba19cec8aa4-ec95a0ed948c-of-bob-ross-hairstyle.jpg',
-        title: 'Sunset in the Cascades',
-        photographer_id: 'smpoutdoors'
+        title: 'A Happy Mountain',
+        artist: 'Bob Ross'
     },
     {
         image_url: 'https://i.ytimg.com/vi/W2bWjbAACZc/maxresdefault.jpg',
-        title: 'Seljalandsfoss, Iceland',
-        photographer_id: 'Robin Kamp'
+        title: 'Cabin by the River',
+        artist: 'Bob Ross'
     },
     {
         image_url: 'https://i.ytimg.com/vi/KhccNW-UpRM/maxresdefault.jpg',
-        title: 'Gods Rays',
-        photographer_id: 'illuminati'
+        title: 'Sunset',
+        artist: 'Bob Ross'
     }
 ];
--- main.js
+++ main.js
@@ -16,4 +16,50 @@ $(document).ready(function(){
 
     });
 
+    /* var carouselContainer = document.querySelector("#carousel");
+
+    var htmlString = carouselImages.map(function(image) {
+
+        return `
+        <div class="carousel-item">
+
+            <div class="change-image-button" id="prev-image-button">
+                <p>
+                    <i class="fas fa-chevron-left"></i>
+                    <i class="fas fa-chevron-left"></i>
+                </p>
+            </div>
+
+            <div id="carousel-image">
+                <img src="${image.image_url}" />
+            </div>
+
+            <div class="change-image-button" id="next-image-button">
+                <p>
+                    <i class="fas fa-chevron-right"></i>
+                    <i class="fas fa-chevron-right"></i>
+                </p>
+            </div>
+
+            <div id="carousel-image-subtitle">
+                <p>"${image.title}" by <span>${image.artist}</span>.</p>
+            </div>
+
+        </div>
+        `
+
+    }).join('');
+
+    console.log(htmlString);
+
+    carouselContainer.innerHTML = htmlString;
+
+    $('#carousel').children('div.carousel-item').addClass('hide-carousel-item');
+
+    $('#carousel').on('click', 'div.change-image-button', function(){
+
+
+
+    }); */
+
 })
